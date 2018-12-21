7091d7a5fcdf533a980e422c9175ee030206b77c
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-12-21 15:51:51 -0200

    RequireDir componente e criação de produto fake

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
-
+const requireDir = require('require-dir');
 
 
 // inicindo o app
@@ -10,12 +9,19 @@ const app = express();
 // iniciando o banco de dados mongodb
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
+requireDir("./src/models");
+
+
+const Product = mongoose.model('Product');
 
 app.get('/', (req, res) => {
+    Product.create({
+        title: "React Aplication",
+        description: "Product is fake for test",
+        url: "http://example.com.br"
+    })
     res.send('Hello Dev Master');
 });
-
-
 
 
 app.listen(3001);
