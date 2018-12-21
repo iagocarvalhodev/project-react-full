d8f0e8a2f6475213f35c0c862877dc86becf135c
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-12-21 16:47:11 -0200

    Controller product, crud completo com as rotas

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,9 @@ const ProductController = require("./controllers/ProductController");
 
 
 routes.get('/products', ProductController.index);
+routes.get('/product/:id', ProductController.show);
+routes.put('/product/:id', ProductController.update);
+routes.post('/products', ProductController.store);
+routes.delete('/product/:id', ProductController.destroy);
 
 module.exports = routes;
