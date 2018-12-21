86385b3061529aba7782a51bc81e25878532680f
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-12-21 15:43:00 -0200

    conexao com banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+
+
+
+// inicindo o app
 const app = express();
+
+// iniciando o banco de dados mongodb
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
 
 app.get('/', (req, res) => {
